a5dbe0ff9c9c2a83a3e7d1bc4d93007981ec43b4
ppss <wh3164335@163.com>
2021-02-01 10:10:09 +0800

    Create StartProductTimestampAssigner.java

---
+++ src/main/java/flinksummary/common/StartProductTimestampAssigner.java
@@ -0,0 +1,27 @@
+package nettyNIO.hander.flink;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+import com.alibaba.fastjson.JSON;
+
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+
+public class StartProductTimestampAssigner implements SerializableTimestampAssigner<String> {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6742651350350360182L;
+
+    @Override
+    public long extractTimestamp(String vo, long timeStamp) {
+        KafkaMessageVo kafkaMessageVo = JSON.parseObject(vo,KafkaMessageVo.class);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime time = LocalDateTime.parse(kafkaMessageVo.getTestTime(), formatter);
+        long timeSecond = time.toInstant(ZoneOffset.of("+8")).toEpochMilli();
+        return timeSecond;
+    }
+    
+}

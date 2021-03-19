53af6ff506672ea1e249f62a68b2707709647ede
ppss <wh3164335@163.com>
2021-03-19 15:09:31 +0800

    Update ProductWindowFunction.java

--- src/main/java/flinksummary/common/ProductWindowFunction.java
+++ src/main/java/flinksummary/common/ProductWindowFunction.java
@@ -6,7 +6,7 @@ import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
-public class ProductWindowFunction implements WindowFunction<Long,RabbitMqVo,KeyVo,TimeWindow> {
+public class ProductWindowFunction implements WindowFunction<Long,KafkaMessageVo,KeyVo,TimeWindow> {
 
     /**
      *
@@ -15,14 +15,14 @@ public class ProductWindowFunction implements WindowFunction<Long,RabbitMqVo,Key
 
     //key为进行keyby的分类
     @Override
-    public void apply(KeyVo key, TimeWindow window, Iterable<Long> input, Collector<RabbitMqVo> out) throws Exception {
-        RabbitMqVo rabbitMqVo = new RabbitMqVo();
-        rabbitMqVo.setJsonId(key.getJsonId());
-        rabbitMqVo.setMouldNoSys(key.getMouldNoSys());
+    public void apply(KeyVo key, TimeWindow window, Iterable<Long> input, Collector<KafkaMessageVo> out) throws Exception {
+        KafkaMessageVo kafkaMessageVo = new KafkaMessageVo();
+        kafkaMessageVo.setJsonId(key.getJsonId());
+        kafkaMessageVo.setMouldNoSys(key.getMouldNoSys());
         String timeStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(window.getStart());
-        rabbitMqVo.setTestTime(timeStr);
-        rabbitMqVo.setNum(input.iterator().next().intValue());
-        out.collect(rabbitMqVo);
+        kafkaMessageVo.setTestTime(timeStr);
+        kafkaMessageVo.setNum(input.iterator().next().intValue());
+        out.collect(kafkaMessageVo);
     }
     
 }
